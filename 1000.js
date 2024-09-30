652f64c2cbd5d7f3087d4800ffe543afa247f0b4
JYJin <jyjin@JYJinui-MacBookAir.local>
2024-09-30 10:09:37 +0900

    Add: 1000.js

--- 1000.js
+++ 1000.js
@@ -1,5 +1,7 @@
-let a;
-let b;
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
 
+const A = parseInt(input[0]);
+const B = parseInt(input[1]);
 
-
+console.log(A + B);
