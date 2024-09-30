8fd9f3d6b19c14281a87a60a73ef25e1df8c9b29
JYJin <jyjin@JYJinui-MacBookAir.local>
2024-09-30 16:40:16 +0900

    Add: 2753.js

---
+++ step 2/2753.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim();
+
+(input % 4 === 0 && input % 100 !== 0) || input % 400 === 0
+  ? console.log(1)
+  : console.log(0);
