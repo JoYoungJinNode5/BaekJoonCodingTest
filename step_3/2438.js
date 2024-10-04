e339d2aa92d6cddea2bed02320ffa90e6dc04f06
JoYoungJinNode5 <jyj950406@naver.com>
2024-10-04 12:20:36 +0900

    Add: 2438.js

--- step_3/2438.js
+++ step_3/2438.js
@@ -2,8 +2,18 @@ const fs = require("fs");
 const input = Number(fs.readFileSync("/dev/stdin").toString().trim());
 
 let result = "";
-for (let i = 1; i <= input; i++) {
-  result += "*".repeat(i) + "\n";
-}
+// for (let i = 1; i <= input; i++) {
+//   result += "*".repeat(i) + "\n";
+// }
 
+// console.log(result.trim());
+
+/**이중 반복문으로 하는 방법*/
+
+for (let i = 1; i <= input; i++) {
+  for (let j = 0; j < i; j++) {
+    result += "*";
+  }
+  result += "\n";
+}
 console.log(result.trim());
