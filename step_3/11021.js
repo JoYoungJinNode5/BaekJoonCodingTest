7277c14dae5ea9006fb26a218130d8a8f6a50855
JoYoungJinNode5 <jyj950406@naver.com>
2024-10-04 11:34:37 +0900

    Add: 11021.js

---
+++ step_3/11021.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const N = parseInt(input[0]);
+let result = "";
+for (let i = 1; i <= N; i++) {
+  const [A, B] = input[i].split(" ").map(Number);
+  result += "Case #" + i + ": " + (A + B) + "\n";
+}
+
+console.log(result);
